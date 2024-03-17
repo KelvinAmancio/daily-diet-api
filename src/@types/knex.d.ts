9332014e4e12f391e7575e6648d32fa5312bb503
KelvinAmancio <kelvin013@gmail.com>
2024-03-17 18:19:48 -0300

    feat: create table for meals

--- src/@types/knex.d.ts
+++ src/@types/knex.d.ts
@@ -1,5 +1,14 @@
 import 'knex'
 
 declare module 'knex/types/tables' {
-    export interface Tables {}
+    export interface Tables {
+        meals: {
+            id: string
+            session_id?: string
+            name: string
+            description: string
+            is_on_diet: boolean
+            created_at: string
+        }
+    }
 }
